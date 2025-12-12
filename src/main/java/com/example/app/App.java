28a7b21084cc00ee5921584553f013654cbf1f90
1vmc1 <vmc0@mail.ru>
2025-12-12 21:46:04 +0300

    Update App.java

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -1,7 +1,21 @@
 package com.example.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins Docker Build!");
+        SpringApplication.run(App.class, args);
+    }
+}
+
+@RestController
+class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Spring Boot on project-server!";
     }
 }
